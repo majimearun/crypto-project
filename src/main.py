8014e8f15dd6b4f0d8f1f7256b9caee46c83953e
majimearun <arunachala0619@gmail.com>
2024-04-21 01:53:16 +0530

    feat: complete io for all users with the blockchain

--- src/main.py
+++ src/main.py
@@ -1,76 +1,22 @@
 import authenticator
 import blockchain
+from seed import Data
+from cryptography.hazmat.primitives import hashes, hmac
 import random
 import student
-import university
-import company
 import course
-from cryptography.hazmat.primitives import hashes, hmac
 
 N_ROUNDS = 1
 DIFFICULTY = 4
-
-# create secret key
 SECRET_KEY = input("Enter secret key: ").encode()
-
 BLOCKCHAIN = blockchain.Blockchain(DIFFICULTY, SECRET_KEY)
 
-# create university
-uni1 = university.University("1", "IIT Bombay")
-uni2 = university.University("2", "IIT Delhi")
-uni3 = university.University("3", "IIT Madras")
-uni4 = university.University("4", "BITS Pilani")
-uni5 = university.University("5", "BITS Hyderabad")
+data = Data()
 
-# create students
-student1 = student.Student("1", "Alice", "Female", 20, "2003-06-24")
-student2 = student.Student("2", "Bob", "Male", 20, "2003-06-24")
-student3 = student.Student("3", "Charlie", "Male", 20, "2003-06-24")
-student4 = student.Student("4", "David", "Male", 20, "2003-06-24")
-student5 = student.Student("5", "Eve", "Female", 20, "2003-06-24")
-
-student_ledger = [student1, student2, student3, student4, student5]
-
-
-# create companies
-company1 = company.Company("Adobe")
-company2 = company.Company("Microsoft")
-company3 = company.Company("Google")
-company4 = company.Company("Facebook")
-company5 = company.Company("Amazon")
-
-company_ledger = [company1, company2, company3, company4, company5]
-
-# create courses
-course1 = course.Course("1", "Blockchain", 3)
-course2 = course.Course("2", "Cryptography", 4)
-course3 = course.Course("3", "AI", 2)
-course4 = course.Course("4", "ML", 5)
-course5 = course.Course("5", "DL", 2)
-
-course_ledger = [course1, course2, course3, course4, course5]
-
-# seed data
-for stud in student_ledger:
-    uni1.add_student(stud)
-    uni2.add_student(stud)
-    uni3.add_student(stud)
-    uni4.add_student(stud)
-    uni5.add_student(stud)
-
-for c in course_ledger:
-    uni1.add_course(c)
-    uni2.add_course(c)
-    uni3.add_course(c)
-    uni4.add_course(c)
-    uni5.add_course(c)
-
-
-# add some transactions
-for i in range(10):
-    uni = random.choice([uni1, uni2, uni3, uni4, uni5])
-    stud = random.choice(student_ledger)
-    course = random.choice(course_ledger)
+for i in range(100):
+    uni = random.choice(data.university_ledger)
+    stud = random.choice(data.student_ledger)
+    course = random.choice(data.course_ledger)
     grade = random.randint(1, 10)
     transaction = blockchain.Transaction(
         uni.university_id, stud.student_id, course.code, grade
@@ -80,30 +26,85 @@ for i in range(10):
     signature = HMAC.finalize()
     BLOCKCHAIN.add_transaction(transaction, signature)
 
-    if i % 2 == 0:
+    if i % 10 == 0:
         BLOCKCHAIN.mine()
 
 
-print("Login as ...\n1. University\n2. Student\n3. Company")
-choice = input("Enter choice: ")
-match choice:
-    case "3":
-        name = input("enter company name: ")
-        flag = False
-        for company in company_ledger:
-            if company.name == name:
-                flag = True
-                break
-        if not flag:
-            print("Company not found")
-            exit()
-        authenticator = authenticator.ChallengeResponseAuthenticator(
-            SECRET_KEY, N_ROUNDS
-        )
-        if authenticator.authenticate(True):
-            print("Authenticated...")
-            print("Access granted")
-            print(f"Welcome {name}")
+def university_login():
+    name = input("Enter university name: ")
+    flag = False
+    for university in data.university_ledger:
+        if university.university_name == name:
+            flag = True
+            break
+    if not flag:
+        print("University not found")
+        return
+
+    auth = authenticator.ChallengeResponseAuthenticator(SECRET_KEY, N_ROUNDS)
+    if auth.authenticate(True):
+        print("Authenticated...")
+        print("Access granted")
+        print(f"Welcome {name}")
+        while True:
+            print("1. Add Student")
+            print("2. Add Course")
+            print("3. Exit")
+            choice = input("Enter choice: ")
+            match choice:
+                case "1":
+                    student_id = input("Enter student id: ")
+                    while True:
+                        for stud in data.student_ledger:
+                            if stud.student_id == student_id:
+                                print("Student already exists")
+                                break
+                        else:
+                            break
+                        student_id = input("Enter student id: ")
+                    name = input("Enter student name: ")
+                    gender = input("Enter gender of student: ")
+                    dob = input("Enter date of birth of student: ")
+                    stud = student.Student(student_id, name, gender, dob)
+                    university.add_student(stud)
+                    data.student_ledger.append(stud)
+                case "2":
+                    course_code = input("Enter course code: ")
+                    while True:
+                        for course in data.course_ledger:
+                            if course.code == course_code:
+                                print("Course already exists")
+                                break
+                        else:
+                            break
+                        course_code = input("Enter course code: ")
+                    name = input("Enter course name: ")
+                    credits = input("Enter credits of course: ")
+                    course = course.Course(course_code, name, credits)
+                    university.add_course(course)
+                    data.course_ledger.append(course)
+                case "3":
+                    return
+                case _:
+                    print("Invalid choice")
+
+
+def company_login():
+    name = input("Enter company name: ")
+    flag = False
+    for company in data.company_ledger:
+        if company.name == name:
+            flag = True
+            break
+    if not flag:
+        print("Company not found")
+        return
+    auth = authenticator.ChallengeResponseAuthenticator(SECRET_KEY, N_ROUNDS)
+    if auth.authenticate(True):
+        print("Authenticated...")
+        print("Access granted")
+        print(f"Welcome {name}")
+        while True:
             print("1. View Student Transcript")
             print("2. Exit")
             choice = input("Enter choice: ")
@@ -113,4 +114,60 @@ match choice:
                     student_id = input("Enter student id: ")
                     print(BLOCKCHAIN.view_student_transcript(university_id, student_id))
                 case "2":
-                    exit()
+                    return
+                case _:
+                    print("Invalid choice")
+
+
+def student_login():
+    university_id = input("Enter university id: ")
+    student_id = input("Enter student id: ")
+    flag = False
+    found_university = None
+    for university in data.university_ledger:
+        if university.university_id == university_id:
+            flag = True
+            found_university = university
+            break
+    if not flag:
+        print("University not found")
+        return
+    flag = False
+    for student in found_university.students:
+        if student.student_id == student_id:
+            flag = True
+            break
+    if not flag:
+        print("Student not found")
+        return
+    auth = authenticator.ChallengeResponseAuthenticator(SECRET_KEY, N_ROUNDS)
+    if auth.authenticate(True):
+        print("Authenticated...")
+        print("Access granted")
+        print(f"Welcome {student_id}")
+        while True:
+            print("1. View Transcript")
+            print("2. Exit")
+            choice = input("Enter choice: ")
+            match choice:
+                case "1":
+                    print(BLOCKCHAIN.view_student_transcript(university_id, student_id))
+                case "2":
+                    return
+                case _:
+                    print("Invalid choice")
+
+
+if __name__ == "__main__":
+    while True:
+        print("Login as ...\n1. University\n2. Student\n3. Company")
+        choice = input("Enter choice: ")
+        match choice:
+            case "1":
+                university_login()
+            case "2":
+                student_login()
+            case "3":
+                company_login()
+            case _:
+                print("Invalid choice")
