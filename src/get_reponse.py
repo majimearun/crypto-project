e02388c63c33b8c4ec7aea9f5b29147dcc0bb102
majimearun <arunachala0619@gmail.com>
2024-04-21 23:02:59 +0530

    fix: use io instead of cli args cz bytes

--- src/get_reponse.py
+++ src/get_reponse.py
@@ -1,18 +1,7 @@
 from cryptography.hazmat.primitives import hashes, hmac
-import argparse
 
-
-parser = argparse.ArgumentParser(description="Get the response for the challenge")
-parser.add_argument(
-    "--secret_key", type=str, default="b'\\x00'", help="The secret key to use for HMAC"
-)
-parser.add_argument(
-    "--challenge", type=str, default="b'\\x00'", help="The challenge to use for HMAC"
-)
-
-args = parser.parse_args()
-challenge = eval(args.challenge)
-secret_key = eval(args.secret_key)
+challenge = eval(input("Enter the challenge: "))
+secret_key = eval(input("Enter the secret key: "))
 
 HMAC = hmac.HMAC(secret_key, hashes.SHA256())
 HMAC.update(challenge)
